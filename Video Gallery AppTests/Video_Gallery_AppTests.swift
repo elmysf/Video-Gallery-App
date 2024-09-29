f405da13834f74724c5977112cdb8dc039c16b40
elmysf <elmysufiandy09@gmail.com>
2024-09-29 12:11:28 +0700

    Initial Commit

---
+++ Video Gallery AppTests/Video_Gallery_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Video_Gallery_AppTests.swift
+//  Video Gallery AppTests
+//
+//  Created by Sufiandy Elmy on 29/09/24.
+//
+
+import Testing
+@testable import Video_Gallery_App
+
+struct Video_Gallery_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Video Gallery AppUITests/Video_Gallery_AppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Video_Gallery_AppUITestsLaunchTests.swift
+//  Video Gallery AppUITests
+//
+//  Created by Sufiandy Elmy on 29/09/24.
+//
+
+import XCTest
+
+final class Video_Gallery_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
